67f8a8b39a4526e5089225e47b21d314b5bca820
ibrahimm963 <mhrbleg771@gmail.com>
2024-10-10 16:41:46 +0200

    Delete android directory

--- android/app/src/main/java/com/example/notification_inshallah/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.notification_inshallah; // Replace with your actual package name
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
