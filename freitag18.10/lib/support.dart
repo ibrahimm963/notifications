78ef371f2b9b2f20d769738444a52ca2d1f92562
ibrahimm963 <mhrbleg771@gmail.com>
2024-10-18 22:43:40 +0200

    support.dart

--- freitag18.10/lib/support.dart
+++ freitag18.10/lib/support.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:settingscreen/main.dart';
 
 class Support extends StatelessWidget {
   const Support({super.key});
@@ -23,8 +24,7 @@ class Support extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.only(
-              top: .0,
-              bottom: 120.0,
+              bottom: 60.0,
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -37,39 +37,89 @@ class Support extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
-                    "Do you need help?",
+                    "Brauchen Sie Hilfe?",
                     style: TextStyle(
-                        fontSize: 34,
+                        fontSize: 24,
                         color: Color(0xFFCDB38B),
                         fontWeight: FontWeight.bold),
                   ),
                 ),
                 Text(
-                  "Contact us!",
+                  "Kontaktieren Sie uns!",
                   style: TextStyle(
-                      fontSize: 34,
+                      fontSize: 24,
                       color: Color(0xFFCDB38B),
                       fontWeight: FontWeight.bold),
                 ),
                 Spacer(),
                 SizedBox(
                   width: 400,
-                  height: 400,
+                  height: 494,
                   child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(28)),
                     child: Column(
                       children: [
                         _containerList(
                             "Adresse", "Soufianstr. 187", Icons.location_on),
+                        Padding(
+                          padding: const EdgeInsets.all(7.5),
+                          child: Container(
+                            width: 337,
+                            height: 1,
+                            color: Color.fromARGB(255, 203, 198, 209),
+                          ),
+                        ),
                         _containerList(
                             "Telefonnummer", "012-3456-789", Icons.phone),
+                        Padding(
+                          padding: const EdgeInsets.all(7.5),
+                          child: Container(
+                            width: 337,
+                            height: 1,
+                            color: Color.fromARGB(255, 203, 198, 209),
+                          ),
+                        ),
                         _containerList(
                             "Email", "Soufian@omarnuh.seyfu", Icons.email),
+                        Padding(
+                          padding: const EdgeInsets.all(7.5),
+                          child: Container(
+                            width: 337,
+                            height: 1,
+                            color: Color.fromARGB(255, 203, 198, 209),
+                          ),
+                        ),
                         _containerList2("AGB", Icons.newspaper,
                             Icons.arrow_forward_ios_outlined),
+                        Padding(
+                          padding: const EdgeInsets.all(7.5),
+                          child: Container(
+                            width: 337,
+                            height: 1,
+                            color: Color.fromARGB(255, 203, 198, 209),
+                          ),
+                        ),
                         _containerList2("Impressum", Icons.newspaper,
                             Icons.arrow_forward_ios_outlined),
+                        Padding(
+                          padding: const EdgeInsets.all(7.5),
+                          child: Container(
+                            width: 337,
+                            height: 1,
+                            color: Color.fromARGB(255, 203, 198, 209),
+                          ),
+                        ),
                         _containerList2("Datenschutz Erklärung",
                             Icons.newspaper, Icons.arrow_forward_ios_outlined),
+                        Padding(
+                          padding: const EdgeInsets.all(7.5),
+                          child: Container(
+                            width: 337,
+                            height: 1,
+                            color: Color.fromARGB(255, 203, 198, 209),
+                          ),
+                        ),
                         _containerList2("App Richtlinien", Icons.newspaper,
                             Icons.arrow_forward_ios_outlined),
                       ],
@@ -90,9 +140,22 @@ class Support extends StatelessWidget {
     IconData icon,
   ) {
     return ListTile(
-      title: Text(title),
-      subtitle: Text(subtitle),
-      leading: Icon(icon),
+      title: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+      subtitle: Text(
+        subtitle,
+        style: TextStyle(
+            color: Colors.grey.shade600,
+            fontSize: 15,
+            fontWeight: FontWeight.w700,
+            fontStyle: FontStyle.italic),
+      ),
+      leading: Icon(
+        icon,
+        color: Color(0xFF551A8B),
+      ),
     );
   }
 
@@ -102,8 +165,14 @@ class Support extends StatelessWidget {
     IconData trailing,
   ) {
     return ListTile(
-      title: Text(title),
-      leading: Icon(icon),
+      title: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+      ),
+      leading: Icon(
+        icon,
+        color: Color(0xFF551A8B),
+      ),
       trailing: Icon(trailing),
     );
   }
